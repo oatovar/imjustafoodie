07a03c9da30ec4cf5424c61adf9eb9b8e318c485
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-05-11 15:12:06 -0500

    Adding initial JS files.

---
+++ js/countdown.js
@@ -0,0 +1,8 @@
+//This will countdown the time to release of the website based on timezone.
+
+function countdownTimer() {
+    let targetMonth = "";
+    let targetDay = "";
+    let targetYear = "";
+    let targetHour = "";
+}
